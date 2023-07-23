e8be1b6dcac365dca8d38bf92273545605ffdd76
Rushikesh Suryawanshi <rushikeshsuryawanshi007@gmail.com>
2023-07-23 13:54:43 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Rushikesh Surywanshi",
+    author_email="rushikeshsurywanshi007@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
